fecf92dd7488a8d82d8850cbbbbc0fb610df6b06
Bartholomew Fountain <baertholomew@gmail.com>
2020-03-12 23:13:37 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,16 @@
+def sort_array_asc (array)
+  array.sort
+end
 
+
+
+def sort_array_desc (array)
+  array.sort {|x,y| y <=> x }
+end
+
+
+
+def swap_elements (array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
